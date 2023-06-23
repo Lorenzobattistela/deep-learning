734922b528da554678ff5d41beaffb5d22301d20
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-06-23 18:01:17 -0300

    [fix] fixing sigmoid function and comments

--- MIT/neural_network.py
+++ MIT/neural_network.py
@@ -3,16 +3,15 @@ import numpy as np
 
 class Layer:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        print(self.weights)
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons) # initialize a matrix n_inputs x n_neurons
         self.biases = np.zeros((1, n_neurons))
-        print(self.biases)
 
     def call(self, inputs):
-        z = np.matmul(inputs, self.weights)
+        z = np.matmul(inputs, self.weights) + self.biases
         output = sigmoid(z)
         return output
 
 
 def sigmoid(z):
-    return 1 / 1 + np.exp(-z)
+    return 1 / (1 + np.exp(-z))
+
